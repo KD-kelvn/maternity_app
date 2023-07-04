be6607ad8252ff418ec8d7f612f70cad4007a145
kevoocodes <97kelvin1@gmail.com>
2023-07-04 18:35:42 +0300

    u

--- database/seeders/DoctorsTableSeeder.php
+++ database/seeders/DoctorsTableSeeder.php
@@ -15,21 +15,21 @@ class DoctorsTableSeeder extends Seeder
     {
         $doctors = [
             [
-                'name' => 'Dr. John Doe',
+                'name' => 'Dr. Kelvin Msindai',
                 'specialty' => 'Cardiology',
-                'email' => 'johndoe@example.com',
-                'phone' => '1234567890',
-                'address' => '123 Main Street, City',
+                'email' => 'kelvin@example.com',
+                'phone' => '0657676767',
+                'address' => 'Kigamboni, Dar es salaam',
                 'birth_date' => '1980-05-15',
                 'gender' => 'Male',
-                'bio' => 'Dr. John Doe is a highly experienced cardiologist...',
+                'bio' => 'Dr. Kelvin Msindai is a highly experienced cardiologist...',
             ],
             [
                 'name' => 'Dr. Jane Smith',
                 'specialty' => 'Pediatrics',
                 'email' => 'janesmith@example.com',
                 'phone' => '9876543210',
-                'address' => '456 Elm Street, City',
+                'address' => '456 Elm Street, Dar es salaam',
                 'birth_date' => '1985-09-21',
                 'gender' => 'Female',
                 'bio' => 'Dr. Jane Smith specializes in providing care for infants and children...',
